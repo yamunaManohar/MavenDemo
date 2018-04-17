237aa3de0c68359a0b7c8e636f73829a56ddf20f
manoh <manoh@Manohar>
2018-04-17 11:30:54 +0530

    first commit

---
+++ MavenDemo/src/qsp/DemoTest.java
@@ -0,0 +1,12 @@
+package qsp;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+	public void testA() {
+		Reporter.log("hiiiiiiiiii",true);
+	}
+
+}
